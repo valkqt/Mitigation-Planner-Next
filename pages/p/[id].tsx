1480c392e2f6809819b454f4f2f331a964035c77
valkqt <bznand@gmail.com>
2025-01-08 15:42:30 +0100

    finished tutorial

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { GetServerSideProps } from "next";
 import ReactMarkdown from "react-markdown";
+import Router from "next/router";
 import Layout from "../../components/Layout";
 import { PostProps } from "../../components/Post";
+import { useSession } from "next-auth/react";
 import prisma from "../../lib/prisma";
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -12,7 +14,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   });
@@ -20,7 +22,28 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     props: post,
   };
 };
+
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: "PUT",
+  });
+  await Router.push("/");
+}
+
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: "DELETE",
+  });
+  Router.push("/");
+}
+
 export default function Post({ post }: PostProps) {
+  const { data: session, status } = useSession();
+  if (status === "loading") {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === post.author?.email;
   let title = post.title;
   if (!post.published) {
     title = `${title} (Draft)`;
@@ -32,10 +55,19 @@ export default function Post({ post }: PostProps) {
         <h2>{title}</h2>
         <p>By {post?.author?.name || "Unknown author"}</p>
         <ReactMarkdown children={post.content} />
+        {!post.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(post.id)}>Publish</button>
+        )}
+        {!post.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(post.id)}>Publish</button>
+        )}
+        {userHasValidSession && postBelongsToUser && (
+          <button onClick={() => deletePost(post.id)}>Delete</button>
+        )}
       </div>
       <style jsx>{`
         .page {
-          background: white;
+          background: var(--geist-background);
           padding: 2rem;
         }
 
