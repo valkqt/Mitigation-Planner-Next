4319e59a8156472e7b625c7d2679b34fe40bc647
Andrea Buzzanca <bznand@gmail.com>
2025-02-16 16:30:12 +0100

    sidebar css

--- components/Encounter/UserControls/Filters/LevelFilter/LevelFilter.tsx
+++ components/Encounter/UserControls/Filters/LevelFilter/LevelFilter.tsx
@@ -9,27 +9,44 @@ interface LevelFilterProps {
 export default function LevelFilter({ onLevelFilter }: LevelFilterProps) {
   const [flags] = useActivationFlagsContext();
   return (
-    <select
-      className={css.dropdown}
-      onChange={(e) => {
-        try {
-          const levelCap = parseInt(e.target.value);
-          onLevelFilter(levelCap);
-        } catch {
-          console.error("bro");
-        }
-      }}
-    >
-      {levelArray.map((level) => (
-        <option
-          value={level}
-          key={level}
-          defaultValue={flags.level}
-          className={css.option}
-        >
-          {level}
-        </option>
-      ))}
-    </select>
+    <div>
+      <input
+        type="range"
+        min={levelArray[0]}
+        max={levelArray[levelArray.length - 1]}
+        step={10}
+        onChange={(e) => {
+          try {
+            const levelCap = parseInt(e.target.value);
+            onLevelFilter(levelCap);
+          } catch {
+            console.log("bro")
+          }
+        }}
+      />
+      <label htmlFor="level">{flags.level}</label>
+    </div>
+    // <select
+    //   className={css.dropdown}
+    //   onChange={(e) => {
+    //     try {
+    //       const levelCap = parseInt(e.target.value);
+    //       onLevelFilter(levelCap);
+    //     } catch {
+    //       console.error("bro");
+    //     }
+    //   }}
+    // >
+    //   {levelArray.map((level) => (
+    //     <option
+    //       value={level}
+    //       key={level}
+    //       defaultValue={flags.level}
+    //       className={css.option}
+    //     >
+    //       {level}
+    //     </option>
+    //   ))}
+    // </select>
   );
 }
