6b8be43b2f13617bd54a4a58cb57be7be77cdac7
valkqt <bznand@gmail.com>
2025-06-13 18:02:15 +0200

    created signout modal

--- components/Navbar/UserArea/LogoutComponent/LogoutComponent.tsx
+++ components/Navbar/UserArea/LogoutComponent/LogoutComponent.tsx
@@ -1,17 +1,49 @@
 import serverSignOut from "@/resources/serverActions/serverSignOut";
 import css from "./LogoutComponent.module.css";
 import { FormEvent } from "react";
+import { useModal } from "@/contexts/ModalContext/ModalContext";
+import useClickOutside from "@/hooks/useClickOutside";
 
 export function LogoutComponent() {
-  async function handleSubmit(e: FormEvent) {
+  // const [_, setShow, __, setModal] = useModal();
+
+  // const ref = useClickOutside<HTMLDivElement>(handleClickOutside);
+
+  // function handleClickOutside(): void {
+  //   setShow(false);
+  // }
+
+  async function handleModalSubmit(e: FormEvent) {
     e.preventDefault();
     await serverSignOut();
     window.location.reload();
   }
 
+  // function handleSubmit(e: FormEvent) {
+  //   e.preventDefault();
+  //   setShow(true);
+  //   console.log("mrow");
+  //   setModal(
+  //     <div ref={ref}>
+  //       <form className={css.modal} onSubmit={(e) => handleModalSubmit(e)}>
+  //         <label>Are you sure?</label>
+  //         <div className={css.modalFooter}>
+  //           <div></div>
+  //           <div style={{ display: "flex", gap: "1rem" }}>
+  //             <button type="submit">Proceed</button>
+  //             <button onClick={() => setShow(false)}>Cancel</button>
+  //           </div>
+  //         </div>
+  //       </form>
+  //     </div>
+  //   );
+  // }
+
   return (
-    <form onSubmit={(e) => handleSubmit(e)}>
-      <button type="submit">Sign out</button>
+    <form onSubmit={(e) => handleModalSubmit(e)}>
+      <button type="submit" className={css.signout}>
+        Sign out
+      </button>
     </form>
   );
 }
